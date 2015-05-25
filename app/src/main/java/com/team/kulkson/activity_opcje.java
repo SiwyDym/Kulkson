dc0eae843b61a512900a8724a098004e3b3accab
DawidNowocien <dawid.nowocien@gmail.com>
2015-05-25 16:13:10 +0200

    nowa

--- app/src/main/java/com/team/kulkson/activity_opcje.java
+++ app/src/main/java/com/team/kulkson/activity_opcje.java
@@ -33,7 +33,7 @@ public class activity_opcje extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml...
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
